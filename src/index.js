404168c3311ebcc8d3cd693d1a59e3b1669f22b9
michael garrison <mgarrison.sa@gmail.com>
2020-05-27 17:09:36 -0500

    completed modules exercise

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ $('body').css('background-color', 'green');
 
 
 const sayHello = (name) => `Hello, ${name}!`;
-
 console.log(sayHello("michael"));
+
+import{hello} from './say-hello';
+console.log(hello);
